31b007177b7c54e6e6018047087bde3fd5b39330
dreew.graphics <teleron42@gmail.com>
2024-11-28 20:30:02 +0800

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+document.addEventListener('keydown', function(e) {
+ 
+  if (e.key === 'F12') {
+      e.preventDefault();
+  }
+ 
+  if (e.ctrlKey && e.shiftKey && e.key === 'I') {
+      e.preventDefault();
+  }
+
+  if (e.ctrlKey && e.key === 'u') {
+      e.preventDefault();
+  }
+});
